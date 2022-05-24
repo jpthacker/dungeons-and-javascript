4cc840d08721fcffe1449f50465fe52ef11cd073
Jack Thacker <jackthacker89@gmail.com>
2022-05-24 12:00:35 +0100

    Copied character creation menu interaction script from project sandbox

--- script.js
+++ script.js
@@ -18,3 +18,110 @@ const abilityBtn = document.querySelectorAll(".ability__btn");
 const backBtn = document.querySelector(".btn--back");
 
 let currentCreationStage = "start";
+
+// Loads the character creation message
+const loadCreationMessage = () => {
+  titleContainer.classList.add("title--top");
+  titleMain.classList.add("title__main--top");
+  menu.classList.remove("menu--start");
+  menu.classList.add("menu");
+  document.querySelector(".menu__title").innerText = "Create Your Adventurer";
+  start.innerHTML = "";
+  createContainer.classList.remove("character-creation--start");
+  backBtn.classList.add("hidden");
+  creationBtn.classList.remove("hidden");
+  creationMessage.innerHTML += `
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa error
+temporibus sint minima iusto quisquam, eveniet architecto, odio quia quas
+ullam laudantium consequuntur deserunt laboriosam perspiciatis suscipit quo
+ducimus nesciunt impedit voluptatibus corrupti. Tempore, eius quae doloribus
+nisi quis corporis sit illum, velit nobis, harum unde dolore at sunt
+possimus.
+`;
+  currentCreationStage = "message";
+  dice(6, 4);
+};
+
+//Loads the name entry form
+const loadNameForm = () => {
+  nameForm.classList.remove("hidden");
+  creationMessage.innerHTML = "";
+  creationMessage.classList.add("hidden");
+  document.querySelector(".menu__title").innerText =
+    "Enter the Name of Your Adventurer";
+  backBtn.classList.remove("hidden");
+  currentCreationStage = "name";
+};
+
+// Loads the class selector
+const loadClassSelector = () => {
+  console.log(playerName);
+  if (playerName !== "") {
+    nameForm.classList.add("hidden");
+    classSelector.classList.remove("hidden");
+    document.querySelector(".menu__title").innerText = "Choose Your Class";
+    errorMessage.innerHTML = "";
+    errorMessage.classList.add("hidden");
+    currentCreationStage = "class";
+  } else {
+    errorMessage.innerHTML = "Please enter a name";
+    errorMessage.classList.remove("hidden");
+  }
+};
+
+// Loads the species selector
+const loadSpeciesSelector = () => {
+  if (playerClass !== "") {
+    console.log(playerClass);
+    classSelector.classList.add("hidden");
+    speciesSelector.classList.remove("hidden");
+    creationMessage.innerHTML = "";
+    creationMessage.classList.add("hidden");
+    document.querySelector(".menu__title").innerText = "Choose Your Species";
+    currentCreationStage = "species";
+  } else {
+    errorMessage.innerHTML = "Please select a class";
+    errorMessage.classList.remove("hidden");
+  }
+};
+
+// Loads the abilities generator
+const loadAbilitiesGenerator = () => {
+  if (playerSpecies !== "") {
+    console.log(playerSpecies);
+    speciesSelector.classList.add("hidden");
+    creationMessage.innerHTML = "";
+    creationMessage.classList.add("hidden");
+    document.querySelector(".menu__title").innerText =
+      "Roll For Your Ability Scores";
+    abilities.classList.remove("hidden");
+    errorMessage.innerHTML = "";
+    errorMessage.classList.add("hidden");
+  } else {
+    errorMessage.innerHTML = "Please select a species";
+    errorMessage.classList.remove("hidden");
+  }
+};
+
+//Handles the character creation menu buttons
+continueBtn.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
+    switch (currentCreationStage) {
+      case "start":
+        loadCreationMessage();
+        break;
+      case "message":
+        loadNameForm();
+        break;
+      case "name":
+        loadClassSelector();
+        break;
+      case "class":
+        loadSpeciesSelector();
+        break;
+      case "species":
+        loadAbilitiesGenerator();
+    }
+  });
+});
